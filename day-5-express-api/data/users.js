c97242d00682982f31e57b9aaebcc83186e03b08
Maina.dev <ianmaina499@gmail.com>
2026-01-30 21:00:50 +0300

    added data folder

---
+++ day-5-express-api/data/users.js
@@ -0,0 +1,8 @@
+export const users = [
+  {
+    id: 1,
+    email: 'admin@example.com',
+    password: 'password123', // plaintext for illustration and learning purposes
+    role: 'admin',
+  },
+]
